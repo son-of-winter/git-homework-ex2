d4c7fceb050423794915eb43a7b1d15de891135d
Anton Shcherbak <anton.scherbak@raiffeisen.ru>
2024-11-19 22:14:00 +0300

    feat: create conflicts

--- Урок 1: Git/src/main.py
+++ Урок 1: Git/src/main.py
@@ -22,6 +22,22 @@ class Customer:
         return f"Customer: {self.name}, Membership: {self.membership}"
 
 
+class GroupOrder("Order"):
+    def __init__(self, customers):
+        super().__init__(customer=None)  # Групповой заказ не привязан к одному клиенту
+        self.customers = customers
+
+    def split_bill(self):
+        if not self.customers:
+            raise ValueError("Нет клиентов для разделения счета.")
+        total = self.final_total()
+        return total / len(self.customers)
+
+    def __str__(self):
+        customer_list = ", ".join([customer.name for customer in self.customers])
+        dish_list = "\n".join([str(dish) for dish in self.dishes])
+        return f"Group Order for {customer_list}:\n{dish_list}\nTotal: ${self.final_total():.2f}"
+
 class Order:
     TAX_RATE = 0.08  # 8% налог
     SERVICE_CHARGE = 0.05  # 5% сервисный сбор
@@ -58,23 +74,6 @@ class Order:
     def __str__(self):
         dish_list = "\n".join([str(dish) for dish in self.dishes])
         return f"Order for {self.customer.name}:\n{dish_list}\nTotal: ${self.final_total():.2f}"
-
-
-class GroupOrder(Order):
-    def __init__(self, customers):
-        super().__init__(customer=None)  # Групповой заказ не привязан к одному клиенту
-        self.customers = customers
-
-    def split_bill(self):
-        if not self.customers:
-            raise ValueError("Нет клиентов для разделения счета.")
-        total = self.final_total()
-        return total / len(self.customers)
-
-    def __str__(self):
-        customer_list = ", ".join([customer.name for customer in self.customers])
-        dish_list = "\n".join([str(dish) for dish in self.dishes])
-        return f"Group Order for {customer_list}:\n{dish_list}\nTotal: ${self.final_total():.2f}"
 
 
 # Пример использования
